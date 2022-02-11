49f9b005e11ade5b65f0ff4ed487c95c0b973d4e
lee <lee@feeditback.co.uk>
2022-02-11 17:50:00 +0000

    sigh

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 // vite.config.js
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { name } from 'package.json';
 
 export default defineConfig({
   // This is required so when hosting on github pages, index.html uses relative paths rather than
   // absolute, which is necessary as the base url is https://cygnut.github.io/spaceman/, not
   // something like https://cygnut.github.io/ - where absolute rather than relative would work.
   //   From https://vitejs.dev/guide/static-deploy.html#github-pages
-  base: '/spaceman/'
+  base: `/${name}/`
 });
