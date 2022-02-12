263885ee6a10278f74d6d7b1b577a4c3bf820aca
Cygnut <ligand@hotmail.co.uk>
2022-02-12 12:19:08 +0000

    relative paths?

--- helpers/meshes.js
+++ helpers/meshes.js
@@ -1,8 +1,8 @@
 import * as three from 'three';
 
 const moonMaterial = new three.MeshStandardMaterial({
-  map: new three.TextureLoader().load('/images/moon.jpg'),
-  normalMap: new three.TextureLoader().load('/images/normal.jpg'),
+  map: new three.TextureLoader().load('./images/moon.jpg'),
+  normalMap: new three.TextureLoader().load('./images/normal.jpg'),
 });
 
 export function addMoon(geometry, position) {
